9516ae458e7396e2b59675345a22d188d8dba588
Udaykiran0929 <udaykirandasari@lyrostech.com>
2024-03-12 14:03:54 +0530

    login page

--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,25 @@
-import React,{useContext} from 'react';
-import Home from './Home';
-import { UserContext } from './App';
+import React, { useContext } from 'react'
+import { UserContext } from './App'
+import './Register.css'
 export default function Register() {
-    const {flag, setFlag} = useContext(UserContext);
+    const {flag,setFlag}=useContext(UserContext);
   return (
     <>
-    <div>Register</div>
-    <button onClick={() => setFlag((prevState) => 1)}>Submit</button>
+    <div className='Register-model'>
+    <div className='Register-model-content'>
+    <div className="Rigister-header">
+          <div className="Register-signup-title">Register</div>
+          <div onClick={() => setFlag(() => 0)} className="close">
+            &times;
+          </div>
+        </div>
+        <input type='text'/><br/>
+        <input type='text'/><br/>
+        <input type='text'/><br/>
+    
+    <button onClick={() => setFlag((prevState) => 2)}>Submit</button>
+    </div>
+    </div>
     </>
   )
 }
